579765043a17d612e1e24758b8182b074ccbfe69
Ajinkya Salunke <ajinkyasalunke90@outlook.com>
2024-11-19 19:13:40 +0530

    Loader added

--- components/Logout.tsx
+++ components/Logout.tsx
@@ -1,12 +1,26 @@
 "use client";
-import { logout } from "@/actions/auth";
+import { useState } from "react";
+import Loader from "./Loader";
 
 const Logout = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleLogout = () => {
+    setIsLoading(true); // Activate the loader
+    setTimeout(() => {
+      console.log("Logged out successfully"); // Simulated logout logic
+      setIsLoading(false); // Disable the loader
+    }, 3000); // Simulate logout delay of 3000ms
+  };
+
+  if (isLoading) {
+    return <Loader />; // Show loader during simulated logout
+  }
+
   return (
     <div
-      onClick={() => logout()}
-      className="bg-gray-600 text-white text-sm
-   px-4 py-2 rounded-md cursor-pointer"
+      onClick={handleLogout}
+      className="bg-gray-600 text-white text-sm px-4 py-2 rounded-md cursor-pointer"
     >
       Logout
     </div>
